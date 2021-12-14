df42fd336469d7623b766888e060e1e4812f944a
Kainulainen Oskari Aleksanteri <oskari.a.kainulainen@student.jyu.fi>
2021-12-14 02:52:21 +0200

    Koodia muokattu ja lisätty kommentit

--- Pallopeli/Pallopeli/Pallopeli.cs
+++ Pallopeli/Pallopeli/Pallopeli.cs
@@ -6,13 +6,42 @@ using Jypeli.Widgets;
 using System;
 using System.Collections.Generic;
 
+
+/// @author  Oskari Kainulainen
+/// @version 1.0
+
 public class Pallopeli : PhysicsGame
 {
-    Vector nopeusVasen = new Vector(-200, 0);
-    Vector nopeusOikea = new Vector(200, 0);
+    Image kauhaKuva = LoadImage("kauha.png");
 
-    PhysicsObject pudotuspallo;
-    PhysicsObject kauha;
+    /// <summary>
+    /// kauhan vasemmalle liikuttamista varten tarvittava vektori
+    /// </summary>
+    private Vector nopeusVasen = new Vector(-200, 0);
+
+    /// <summary>
+    /// kauhan oikealle liikuttamista varten tarvittava vektori
+    /// </summary>
+    private Vector nopeusOikea = new Vector(200, 0);
+
+
+    /// <summary>
+    /// pudotuspallon teko atribuutiksi
+    /// </summary>
+    private PhysicsObject pudotuspallo;
+
+    /// <summary>
+    /// kauhan teko atribuutiksi
+    /// </summary>
+    private PhysicsObject kauha;
+
+
+    /// <summary>
+    /// elämäpistelaskuri
+    /// </summary>
+    IntMeter elamat;
+
+
     public override void Begin()
     {
         LuoKentta();
@@ -24,10 +53,16 @@ public class Pallopeli : PhysicsGame
         Add(alkuvalikko);
     }
 
+    /// <summary>
+    /// Luodaan kenttä, kauhat sekä elämäpistelaskuri
+    /// </summary>
     public void LuoKentta()
     {
-
-        Level.CreateBorders(1.0, false);
+        Level.CreateLeftBorder();
+        Level.CreateRightBorder();
+        Level.CreateTopBorder();
+        PhysicsObject alaReuna = Level.CreateBottomBorder();
+        alaReuna.Tag = "ala";
 
         Level.Background.Color = Color.Black;
 
@@ -40,16 +75,22 @@ public class Pallopeli : PhysicsGame
         //luo kentän
         ruudut.Execute(34.2, 25);
 
-        void LuoPallo(Vector paikka, double leveys, double korkeus)
-            {
-                PhysicsObject pallo = PhysicsObject.CreateStaticObject(40, 40);
-                pallo.Shape = Shape.Circle;
-                pallo.Color = Color.Gray;
-                pallo.Position = paikka;
-                pallo.Restitution = 1.0;
-                Add(pallo);
-            }
 
+        /// <summary>
+        /// luodaan kenttää TitleMapin mukaan
+        /// </summary>
+        void LuoPallo(Vector paikka, double leveys, double korkeus)
+        {
+            PhysicsObject pallo = PhysicsObject.CreateStaticObject(40, 40);
+            pallo.Shape = Shape.Circle;
+            pallo.Color = Color.Gray;
+            pallo.Position = paikka;
+            pallo.Restitution = 1.0;
+            pallo.KineticFriction = 0.0;
+            Add(pallo);
+        }
+
+        LuoElamat();
 
         kauha = LuoKauha(0.0, Level.Bottom + 10);
 
@@ -57,7 +98,28 @@ public class Pallopeli : PhysicsGame
 
     }
 
-    PhysicsObject LuoKauha(double x, double y)
+    /// <summary>
+    /// luodaan elämäpistelaskuri
+    /// </summary>
+    void LuoElamat()
+    {
+        elamat = new IntMeter(3);
+
+        Label elamanaytto = new Label();
+        elamanaytto.X = Screen.Right - 10;
+        elamanaytto.Y = Screen.Top - 10;
+        elamanaytto.TextColor = Color.White;
+
+        elamanaytto.BindTo(elamat);
+        Add(elamanaytto);
+    }
+
+    /// <summary>
+    /// luodaan peliin kauha
+    /// </summary>
+    /// <param name="x">kauhan sijainti x-akselilla</param>
+    /// <param name="y">kauhan sijainti y-akselilla</param>
+    private PhysicsObject LuoKauha(double x, double y)
     {
         kauha = PhysicsObject.CreateStaticObject(100, 15);
         kauha.Shape = Shape.Rectangle;
@@ -65,29 +127,79 @@ public class Pallopeli : PhysicsGame
         kauha.X = x;
         kauha.Y = y;
         kauha.Restitution = 1.0;
+        kauha.KineticFriction = 0.0;
+        kauha.Image = kauhaKuva;
         Add(kauha);
         return kauha;
     }
 
-    PhysicsObject LuoPudotuspallo(double x)
+
+    /// <summary>
+    /// Luodaan pudotuspallo
+    /// </summary>
+    /// <param name="x">pallon sijainti x-akselilla</param>
+    public PhysicsObject LuoPudotuspallo(double x)
     {
         pudotuspallo = new PhysicsObject(15, 15);
         pudotuspallo.Shape = Shape.Circle;
         pudotuspallo.Color = Color.White;
         pudotuspallo.X = x;
-        pudotuspallo.Y = 390;
+        pudotuspallo.Y = Screen.Top - 10;
         pudotuspallo.Restitution = 1.0;
+        pudotuspallo.KineticFriction = 0.0;
         Add(pudotuspallo);
+        AddCollisionHandler(pudotuspallo, "ala", Huti);
         return pudotuspallo;
     }
 
-    void AloitaPeli()
+
+    /// <summary>
+    /// Jos pallo pääse kauhan ohi, niin vähennetään elämäpistelaskurista piste ja luodaan uusi pudotuspallo.
+    /// Jos elämäpisteet loppuvat niin lopetetaan peli.
+    /// </summary>
+    /// <param name="pallo">pallo jota seurataan</param>
+    /// <param name="kohde"></param>
+    public void Huti(PhysicsObject pallo, PhysicsObject kohde)
     {
-        LuoPudotuspallo(20);
-        pudotuspallo.Hit(new Vector(-60, -80));
+        pallo.Destroy();
+        elamat.Value -= 1;
+        if (elamat.Value == 0)
+        {
+            MessageDisplay.Add("Kuolit");
+            Keyboard.Disable(Key.A);
+            Keyboard.Disable(Key.D);
+            kauha.Velocity = Vector.Zero;
+            MultiSelectWindow alkuvalikko = new MultiSelectWindow("Pallopeli", "Aloita peli", "Lopeta");
+            alkuvalikko.AddItemHandler(1, Exit);
+            alkuvalikko.AddItemHandler(0, AloitaPeli);
+            Add(alkuvalikko);
+        }
+        else
+        {
+            LuoPudotuspallo(20);
+            pudotuspallo.Hit(new Vector(RandomGen.NextInt(-100, 100), -60));
+        }
+
     }
 
-    public void AsetaOhjaimet()
+
+    /// <summary>
+    /// Aloitetaan peli ja nollataan elämäpistelaskuri
+    /// </summary>
+    void AloitaPeli()
+    {
+        Keyboard.Enable(Key.A);
+        Keyboard.Enable(Key.D);
+        elamat.Value = 3;
+        LuoPudotuspallo(20);
+        pudotuspallo.Hit(new Vector(RandomGen.NextInt(-100, 100), -60));
+    }
+
+
+    /// <summary>
+    /// Asetetaan ohjelma kuuntelemaan näppäinpainalluksia
+    /// </summary>
+    private void AsetaOhjaimet()
     {
         Keyboard.Listen(Key.A, ButtonState.Down, LiikutaKauhaa, "Liikuta kauhaa vasemmalle", kauha, nopeusVasen);
         Keyboard.Listen(Key.A, ButtonState.Released, LiikutaKauhaa, null, kauha, Vector.Zero);
@@ -96,9 +208,17 @@ public class Pallopeli : PhysicsGame
 
         Keyboard.Listen(Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä ohjeet");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.S, ButtonState.Pressed, palloJumissa, "Jos pallo jää jumiin");
+
     }
 
-    void LiikutaKauhaa(PhysicsObject kauha, Vector nopeus)
+
+    /// <summary>
+    /// Aliohjelmalla liikutetaan kauhaa
+    /// </summary>
+    /// <param name="kauha">liikutettava kohde</param>
+    /// <param name="nopeus">nopeus jolla kauha liikkuu</param>
+    private void LiikutaKauhaa(PhysicsObject kauha, Vector nopeus)
     {
         if ((nopeus.X < 0) && (kauha.Left < Level.Left))
         {
@@ -114,6 +234,17 @@ public class Pallopeli : PhysicsGame
         kauha.Velocity = nopeus;
     }
 
+
+    /// <summary>
+    /// Pallon jäädessä jumiin tönäistään sitä jolloin peli voi taas jatkua
+    /// </summary>
+    public void palloJumissa()
+    {
+        pudotuspallo.Hit(new Vector(RandomGen.NextInt(-100, 100), 0));
+    }
+
+
 }
+
 
 
